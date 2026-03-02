958af7f0fbf8d288d7f657aa3bf2eb3ba5656ba6
markTimblau <2024_mark.timblau@iticbcn.cat>
2026-03-02 17:26:31 +0100

    Comienzo consultas basicas

--- projecte2/src/main/java/com/ra34/projecte2/controller/ProductController.java
+++ projecte2/src/main/java/com/ra34/projecte2/controller/ProductController.java
@@ -1,5 +1,32 @@
 package com.ra34.projecte2.controller;
 
-public class ProductController {
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+import com.ra34.projecte2.model.Product;
 
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @GetMapping
+    //Consultar tots els productes
+
+    @GetMapping
+    public List<Product> getProductById() {
+    }
+
+    @AddMapping
+    //Afegir un producte
+
+    @PutMapping
+    //Modificar tots els camps d’un producte
+    /*
+
+Modificar l’estoc de productes
+Modificar el preu d’un producte
+Borrat físic d'un producte
+Borrat lògic d’un producte
+
+     */
 }
