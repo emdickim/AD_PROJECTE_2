b7d6c4217978d242fd05eddfc611c05694047d9d
emdickim <kimlazaropardo@gmail.com>
2026-04-24 13:20:09 +0200

    FET punt 1 integrrant 1

--- projecte2/projecte2/src/main/java/com/ra34/projecte2/Controller/UserController.java
+++ projecte2/projecte2/src/main/java/com/ra34/projecte2/Controller/UserController.java
@@ -1,5 +1,31 @@
 package com.ra34.projecte2.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ra34.projecte2.Model.CreateUserRequest;
+import com.ra34.projecte2.Model.User;
+import com.ra34.projecte2.Service.UserService;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    public ResponseEntity<?> createUser(@RequestBody CreateUserRequest request) {
+        try {
+            User createdUser = userService.createUser(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error" + e.getMessage());
+        }
+    }
 }
--- projecte2/projecte2/src/main/java/com/ra34/projecte2/Repository/UserRepository.java
+++ projecte2/projecte2/src/main/java/com/ra34/projecte2/Repository/UserRepository.java
@@ -1,5 +1,15 @@
 package com.ra34.projecte2.Repository;
 
-public class UserRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ra34.projecte2.Model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email); 
 
 }
